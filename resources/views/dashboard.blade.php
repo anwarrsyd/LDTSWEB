56ee211ef0eb02db343299c968dd0d7a81728c13
anwarrsyd <anwarrosyidi.5@gmail.com>
2016-04-10 22:18:43 +0700

    sudah bisa search box gan!!! cuma belum bisa login

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -30,16 +30,70 @@
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
     <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-    <title>Google Maps Location picker</title>
-    <style type="text/css">    
+    
+ <style>
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
       #map {
         margin: 10px;
         width: 600px;
         height: 300px;  
         padding: 10px;
       }
-</style>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCK2E3Dd2UxSPHSogXf3tzdIoy3BftPK-o&signed_in=true&callback=initMap"
+.controls {
+  margin-top: 10px;
+  border: 1px solid transparent;
+  border-radius: 2px 0 0 2px;
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  height: 32px;
+  outline: none;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+}
+
+#pac-input {
+  background-color: #fff;
+  font-family: Roboto;
+  font-size: 15px;
+  font-weight: 300;
+  margin-left: 12px;
+  padding: 0 11px 0 13px;
+  text-overflow: ellipsis;
+  width: 300px;
+}
+
+#pac-input:focus {
+  border-color: #4d90fe;
+}
+
+.pac-container {
+  font-family: Roboto;
+}
+
+#type-selector {
+  color: #fff;
+  background-color: #4d90fe;
+  padding: 5px 11px 0px 11px;
+}
+
+#type-selector label {
+  font-family: Roboto;
+  font-size: 13px;
+  font-weight: 300;
+}
+
+    </style>
+    <title>Places Searchbox</title>
+    <style>
+      #target {
+        width: 345px;
+      }
+    </style>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCK2E3Dd2UxSPHSogXf3tzdIoy3BftPK-o&signed_in=true&callback=initAutocomplete"
     type="text/javascript"></script>
 
 </head>
@@ -146,6 +200,7 @@
                                     </div>
                                         
                                         
+                                    <input id="pac-input" class="controls" type="text" placeholder="Search Box">
                                     <div id="map"></div>
 
                                     <div class="row">
@@ -234,27 +289,32 @@
     	});
 	</script>
 
-    <script>
-        function updateMarkerPosition(latLng) {
+    
+ <script>
+    function updateMarkerPosition(latLng) {
       document.getElementById('latitude').value = [latLng.lat()]
-        document.getElementById('longitude').value = [latLng.lng()]
+    document.getElementById('longitude').value = [latLng.lng()]
     }
-           
-    var map = new google.maps.Map(document.getElementById('map'), {
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+
+function initAutocomplete() {
+var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: new google.maps.LatLng(-7.279357540138927,112.79737586238673),
      mapTypeId: google.maps.MapTypeId.ROADMAP
       });
-    //posisi awal marker
-//     if (navigator.geolocation) {
-//    navigator.geolocation.getCurrentPosition(function(position) {
-//      var latLng= {
-//        lat: position.coords.latitude,
-//        lng: position.coords.longitude
-//      };   
-//  }
-// }
-     var latLng = new google.maps.LatLng(-7.279357540138927,112.79737586238673);
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+    var latLng = new google.maps.LatLng(-7.279357540138927,112.79737586238673);
      
     /* buat marker yang bisa di drag lalu 
       panggil fungsi updateMarkerPosition(latLng)
@@ -274,6 +334,72 @@
         updateMarkerPosition(marker.getPosition());
       });
 
-    </script>
 
+  var markers = [];
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+        var marker = new google.maps.Marker({
+        position :  place.geometry.location,
+        title : 'lokasi',
+        map : map,
+        draggable : true
+      });
+       
+    updateMarkerPosition( place.geometry.location);
+    google.maps.event.addListener(marker, 'drag', function() {
+     // ketika marker di drag, otomatis nilai latitude dan longitude
+     //menyesuaikan dengan posisi marker 
+        updateMarkerPosition(marker.getPosition());
+      });
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+
+  // [END region_getplaces]
+}
+
+
+    </script>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCK2E3Dd2UxSPHSogXf3tzdIoy3BftPK-o&libraries=places&callback=initAutocomplete"
+         async defer></script>
+    
 </html>
